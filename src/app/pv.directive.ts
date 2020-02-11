083ca27a56ef0b2db589bb76bf8cf152bf51d348
Biel Serrano Sanchez <bisesa1920daw2@gmail.com>
2020-02-11 20:02:20 +0100

    Directiva preview

---
+++ src/app/pv.directive.ts
@@ -0,0 +1,8 @@
+import { Directive, ViewContainerRef } from "@angular/core";
+
+@Directive({
+  selector: "[preview-host]"
+})
+export class PreviewDirective {
+  constructor(public viewContainerRef: ViewContainerRef) {}
+}
